dea9cde2619ced634503fcde37fbc65171a586c9
Its-me-a-guy <102881579+Its-me-a-guy@users.noreply.github.com>
2022-07-22 09:54:12 -0300

    Update Achievements.hx

--- source/Achievements.hx
+++ source/Achievements.hx
@@ -11,22 +11,22 @@ using StringTools;
 
 class Achievements {
 	public static var achievementsStuff:Array<Dynamic> = [ //Name, Description, Achievement save tag, Hidden achievement
-		["that's a wonderful idea",	"Jogue numa Sexta... A noite... YEAH.",						'friday_night_play',	 true],
-		["As ruínas",		"Vença a week 1 no Hard sem errar.",				'week1_nomiss',			false],
-		["Bem-vindo a snowdin",				"Vença a week 2 no Hard sem errar.",				'week2_nomiss',			false],
-		["NGAAAAH",			"Vença a week 3 no Hard sem errar.",				'week3_nomiss',			false],
-		["Oh yes",					"Vença a week 4 no Hard sem errar.",				'week4_nomiss',			false],
-		["A barreira",			"Vença a week 5 no Hard sem errar.",				'week5_nomiss',			false],
-		["Seu pior pesadelo",					"Vença a week 6 no Hard sem errar.",				'week6_nomiss',			false],
-		["Kekeke",			"Ache a cutscene secreta dentro do game.",				'week7_nomiss',			 true],
-		["Seu idiota",	"Acerte menos que 20% da musica.",	'ur_bad',				false],
-		["Eita uau como você é bom amigo",				"tenha 100% em uma musica, confio em ti.",			'ur_good',				false],
-		["os espectadores estão subindo",			"veja os espectadores subirem 10 vezes.",			'roadkill_enthusiast',	false],
-		["Uau... Você canta bastante hein",		"Segure uma nota por 20 segundos",					'oversinging',			false],
-		["Faz sentido como voce caiu",					"Termine uma musica sem voltar para a animacao padrao.",				'hype',					false],
-		["Muffet...?",			"Termine uma musica com apenas duas teclas.",			'two_keys',				false],
-		["Gameboy",				"Por acaso, voce jogou o mod num gameboy?",		'toastie',				false],
-		["Hacker sujo",					"Ganhe o \"Test\" pelo Editor de Chart.",	'debugger',				 true]
+		["Freaky on a Friday Night",	"Jogue numa Sexta... A noite... YEAH.",						'friday_night_play',	 true],
+		["Boa tentativa velhote",		"Vença a week 1 no Hard sem errar.",				'week1_nomiss',			false],
+		["Sem mais travessuras só doçuras",				"Vença a week 2 no Hard sem errar.",				'week2_nomiss',			false],
+		["Me chame de o matador",			"Vença a week 3 no Hard sem errar.",				'week3_nomiss',			false],
+		["Até em alta velocidade",					"Vença a week 4 no Hard sem errar.",				'week4_nomiss',			false],
+		["Um natal sem vacilos",			"Vença a week 5 no Hard sem errar.",				'week5_nomiss',			false],
+		["A mim que elas querem agora!!",					"Vença a week 6 no Hard sem errar.",				'week6_nomiss',			false],
+		["[[HIPERLINK BLOCKED]]",			"Ache a cutscene secreta dentro do game.",				'week7_nomiss',			 true],
+		["Isso foi um Funkin' Disastre!",	"Acerte menos que 20% da musica.",	'ur_bad',				false],
+		["Perfecionista",				"tenha 100% em uma musica, confio em ti.",			'ur_good',				false],
+		["Roadkill Enthusiast",			"Watch the Henchmen die over 100 times.",			'roadkill_enthusiast',	false],
+		["Cantando, mas tipo MUITO...?",		"Segure uma nota por 20 segundos",					'oversinging',			false],
+		["Hiperativo",					"Termine uma musica sem voltar para a animacao padrao.",				'hype',					false],
+		["Só nós dois",			"Termine uma musica com apenas duas teclas.",			'two_keys',				false],
+		["Torradeira Gamer",				"Por acaso, voce jogou o mod numa torradeira?",		'toastie',				false],
+		["Debugger",					"Ganhe o \"Test\" pelo Editor de Chart.",	'debugger',				 true]
 	];
 	public static var achievementsMap:Map<String, Bool> = new Map<String, Bool>();
 
